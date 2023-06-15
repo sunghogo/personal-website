9c3717dbc11f10337ded084458120f2f352c3528
sunghogo <leinad5592@gmail.com>
2023-06-15 12:50:54 -0400

    Header.js: Added responsive padding

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Typography from "./Typography";
 function Header({ children }) {
   return (
     <Typography
-      className="header-image font-montserrat-header text-gray-900 mb-4 flex justify-center border border-2 border-gray-100 rounded-full w-1/2 mx-auto"
+      className="header-image font-montserrat-header text-gray-900 mb-4 sm:py-1 md:py-2 lg:py-4 flex justify-center border border-2 border-gray-100 rounded-full w-1/2 mx-auto"
       strong
       em
       h1
