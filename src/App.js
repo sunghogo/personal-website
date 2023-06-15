f4caf699fe9c8f98242584048fa5a997e56fbac1
sunghogo <leinad5592@gmail.com>
2023-06-15 12:07:00 -0400

    Created Pages for each different website sections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Container from "./components/Container";
+import AboutPage from "./pages/AboutPage";
+import SkillsPage from "./pages/SkillsPage";
+import ProjectsPage from "./pages/ProjetsPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
     <div className="background-image flex flex-wrap grid px-8 md:px-16 py-4">
-      <Container />
-      <Container />
-      <Container />
-      <Container />
+      <AboutPage />
+      <SkillsPage />
+      <ProjectsPage />
+      <ContactPage />
     </div>
   );
 }
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,54 @@
+import Container from "../components/Container";
+
+function AboutPage() {
+  const header = `About Me`;
+  const text = ` Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed mattis
+    ante, quis pretium leo. Quisque nec neque nec ante finibus sagittis a
+    sed arcu. Maecenas molestie sollicitudin lectus, ac elementum justo.
+    Suspendisse nibh augue, eleifend id metus non, gravida viverra nibh.
+    Fusce non magna nisl. Vestibulum aliquet, metus eu egestas sodales,
+    augue nisl commodo sapien, ut ornare justo purus sed libero. Fusce
+    libero nunc, ornare eu erat vitae, ullamcorper pulvinar ante. Sed dui
+    mi, varius ac laoreet eget, aliquam ac justo. Ut nibh felis, pretium ut
+    nisi vel, iaculis varius quam. Vestibulum facilisis sodales odio, vitae
+    mollis augue imperdiet in. Duis pharetra orci vitae diam mollis maximus.
+    Aliquam finibus porttitor nisl, vitae laoreet lacus dapibus sed.
+    Suspendisse bibendum molestie elit, imperdiet fermentum purus rhoncus
+    vel. Duis vitae neque dictum, lacinia metus quis, consequat ipsum.
+    Vivamus tellus lorem, tristique sit amet metus ut, consequat viverra mi.
+    Nunc dictum, turpis ut consectetur sagittis, risus nunc finibus erat,
+    cursus hendrerit mi nisl vel augue. Nulla facilisi. In hac habitasse
+    platea dictumst. Mauris bibendum nunc at massa malesuada, eu facilisis
+    ex rhoncus. Curabitur sed viverra neque. Integer imperdiet viverra risus
+    elementum viverra. Suspendisse potenti. Nam eleifend imperdiet maximus.
+    Sed tincidunt nibh ac est ultricies, ac tincidunt nunc tristique.
+    Praesent sed fringilla leo. Morbi in lectus suscipit, consectetur urna
+    at, faucibus mi. In sollicitudin, elit ac commodo commodo, sem ex
+    bibendum enim, vitae imperdiet ex purus vel ligula. Phasellus eget purus
+    laoreet lectus blandit rhoncus in pretium purus. Maecenas rhoncus
+    placerat sem sed volutpat. Curabitur quis turpis in nisi ornare finibus
+    eget convallis nulla. Sed nisl nibh, lacinia vitae eros ac, faucibus
+    ultricies lacus. Nullam ut ligula quis nulla consectetur efficitur sit
+    amet quis leo. Fusce venenatis augue tortor, nec fringilla risus
+    faucibus in. Vivamus convallis congue purus id porttitor. Donec lorem
+    nulla, accumsan semper nulla vel, iaculis congue neque. Mauris aliquet
+    vehicula ex sed bibendum. Duis ultrices semper erat, et fermentum neque
+    suscipit sed. Sed in semper felis. Nunc gravida mattis leo, eget semper
+    nisl fermentum vitae. Quisque volutpat ipsum ac sapien rutrum dignissim.
+    Fusce turpis tortor, rutrum ac dignissim at, sagittis sit amet urna.
+    Nulla vitae faucibus ligula. Praesent ornare vehicula ligula. Vivamus
+    consectetur pulvinar facilisis. Nam elementum eros at porta condimentum.
+    Maecenas mattis vel ex vel gravida. Quisque feugiat tincidunt sapien
+    eget euismod. Curabitur accumsan porttitor sapien in elementum. Aenean
+    dapibus mi odio, vel cursus lorem pretium in. Duis aliquam facilisis
+    nisi, sit amet feugiat orci venenatis in. Maecenas auctor viverra augue
+    eget dictum. Suspendisse ut ultricies nisl, vel hendrerit risus.
+    Curabitur molestie sit amet nulla id lacinia. Maecenas varius, diam et
+    euismod commodo, libero augue eleifend nibh, nec luctus velit purus sed
+    nibh. Praesent commodo, lectus ac posuere commodo, enim leo molestie
+    velit, in elementum dolor turpis sed tellus.`;
+
+  return <Container header={header} text={text} />;
+}
+
+export default AboutPage;
