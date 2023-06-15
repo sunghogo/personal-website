5784d207050384b4899e23b6638e32c7f4ee83e9
sunghogo <leinad5592@gmail.com>
2023-06-15 13:09:10 -0400

    index.js: Wrapped App component with NavigationProvider to use NavigationContext

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { NavigationProvider } from "./context/NavigationContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
+);
