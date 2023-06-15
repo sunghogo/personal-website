d1251e929c5cc1167e510ae5ac682784fee59d7d
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-06-14 23:45:01 -0400

    App.js: Added flex styling to prevent child components from overflowing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Container from "./components/Container";
 
 function App() {
   return (
-    <div className="background-image px-8">
+    <div className="background-image flex flex-wrap grid px-8 md:px-16 py-4">
       <Container />
       <Container />
       <Container />
