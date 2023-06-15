4f703cc40847bab814fccdb8e345fa3b311a4220
sunghogo <leinad5592@gmail.com>
2023-06-15 12:51:51 -0400

    Container.js: Switched to single, thinner border

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 function Container({ header, text }) {
   return (
-    <div className="container mx-auto my-4 px-8 py-4 border-double border-4 rounded-lg borderr-gray-200 shadow-lg bg-white">
+    <div className="container mx-auto my-4 px-8 py-4 border border-2 rounded-lg borderr-gray-200 shadow-lg bg-white">
       <Header>{header}</Header>
 
       <Typography className="font-open-sans text-gray-700">{text}</Typography>
